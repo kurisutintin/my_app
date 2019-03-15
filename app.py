6291a062f1ee63e86785c11eac7dd2ab97e16864
Nadya <n.bublik@warwick.ac.uk>
2019-03-15 15:46:43 +0000

    Big update

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 import requests
 app = Flask("my_app")
 
-def send_email(senders_emai_addressl,senders_name):
+def send_email(senders_email_address, senders_name):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox9bec1d3584a44bcdbd6039001706cc67.mailgun.org/messages",
         auth=("api", "e6fffe3a767781dfdb962946a862d0c8-de7062c6-df31c160"),
         data={"from": "Christine Ho <postmaster@sandbox9bec1d3584a44bcdbd6039001706cc67.mailgun.org>",
-              "to": "senders_email_address",
-              "subject": "Thank you for subscribing!",
+              "to": senders_email_address,
+              "subject": "Thank you for subscribing, {}!".format(senders_name),
               "text": "Congratulations!  You have subscribed!"})
 # You can see a record of this email in your logs: https://app.mailgun.com/app/logs
 
@@ -16,15 +16,15 @@ def send_email(senders_emai_addressl,senders_name):
 def say_hello():
     return render_template("index.html")
 
-@app.route("/News.html")
+@app.route("/news")
 def News():
     return render_template("News.html")
 
-@app.route("/Weathers.html")
+@app.route("/weather")
 def Weathers():
     return render_template("Weathers.html")
 
-@app.route("/TV.html")
+@app.route("/TV")
 def TV():
     return render_template("TV.html")
 
